3d3e34ea4f2685f1750313f3ae36140cfcb4a254
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2021-12-06 11:25:01 -0600

    Day 6

--- Advent of Code/Year 2021/Day_06.cs
+++ Advent of Code/Year 2021/Day_06.cs
@@ -6,13 +6,47 @@
         {
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            Int64[] fishTimerCounts = new Int64[9];
+
+            // Load initial fish timers
+            var fishTimers = input.ReadLine().Split(',').Select(n => int.Parse(n)).ToList();
+            foreach (var fishTimer in fishTimers)
             {
+                fishTimerCounts[fishTimer]++;
+            }
+
+            // Initialize fish counts
+            Int64 fishCountInitial = fishTimerCounts.Sum();
+            Int64 fishCount80Days = 0;
+            Int64 fishCount256Days = 0;
+
+            // Loop through each day
+            for (var day = 0; day < 256; day++)
+            {
+                var birthingFishCount = fishTimerCounts[0];
+                for (var fishTimerIndex = 0; fishTimerIndex < 8; fishTimerIndex++)
+                {
+                    fishTimerCounts[fishTimerIndex] = fishTimerCounts[fishTimerIndex + 1];
+                }
+                fishTimerCounts[6] += birthingFishCount;
+                fishTimerCounts[8] = birthingFishCount;
+
+                if (day + 1 == 80)
+                {
+                    fishCount80Days = fishTimerCounts.Sum();
+                }
+                else if (day + 1 == 256)
+                {
+                    fishCount256Days = fishTimerCounts.Sum();
+                }
             }
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"Initially {fishCountInitial:N0} fish\r\n" +
+                   $"{fishCount80Days:N0} fish after 80 days\r\n" +
+                   $"{fishCount256Days:N0} fish after 256 days\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
 
     }
