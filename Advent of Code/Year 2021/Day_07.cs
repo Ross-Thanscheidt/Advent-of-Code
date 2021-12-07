3d9d79bd34e9b2b1e4e60680aef852e3077023a2
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2021-12-07 11:31:47 -0600

    Year 2021 Day 7

--- Advent of Code/Year 2021/Day_07.cs
+++ Advent of Code/Year 2021/Day_07.cs
@@ -6,13 +6,39 @@
         {
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            var positions = input.ReadLine().Split(',').Select(n => int.Parse(n)).ToList();
+
+            var minFuelUsedConstant = int.MaxValue;
+            var minFuelPositionConstant = 0;
+            var minFuelUsedAccelerated = int.MaxValue;
+            var minFuelPositionAccelerated = 0;
+            for (var position = positions.Min(); position <= positions.Max(); position++)
             {
+                var currentPositionFuelConstant = positions.Sum(n => Math.Abs(n - position));
+                if (currentPositionFuelConstant < minFuelUsedConstant)
+                {
+                    minFuelUsedConstant = currentPositionFuelConstant;
+                    minFuelPositionConstant = position;
+                }
+
+                var currentPositionFuelAccelerated = positions.Sum(n =>
+                    {
+                        var dx = Math.Abs(n - position);
+                        return dx * (dx + 1) / 2;
+                    });
+                if (currentPositionFuelAccelerated < minFuelUsedAccelerated)
+                {
+                    minFuelUsedAccelerated = currentPositionFuelAccelerated;
+                    minFuelPositionAccelerated = position;
+                }
             }
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"{positions.Count():N0} horizontal positions from {positions.Min():N0} to {positions.Max():N0}\r\n" +
+                   $"Position {minFuelPositionConstant:N0} costs the least fuel ({minFuelUsedConstant:N0}) for constant fuel rate movement\r\n" +
+                   $"Position {minFuelPositionAccelerated:N0} costs the least fuel ({minFuelUsedAccelerated:N0}) for accelerated fuel rate movement\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
 
     }
