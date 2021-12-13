7163b534b67353742b732ec4de49da8a04a045fc
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2021-12-13 11:28:05 -0600

    Year 2021 Day 11

--- Advent of Code/Year 2021/Day_11.cs
+++ Advent of Code/Year 2021/Day_11.cs
@@ -1,4 +1,6 @@
-﻿namespace Advent_of_Code
+﻿using Advent_of_Code.Year_2021_Day_11;
+
+namespace Advent_of_Code
 {
     public partial class Year_2021 : IYear
     {
@@ -6,14 +8,53 @@
         {
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            // Get input lines
+            var lines = new List<string>();
+            for (var line = input.ReadLine()?.Trim(); line != null; line = input.ReadLine())
             {
+                if (line.Length > 0)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            // Load array of dumbo octopus energy levels into EnergyGrid object
+            var energyGrid = new EnergyGrid(lines.Count, lines[0].Length);
+            foreach (var line in lines)
+            {
+                var energyLevelsRow = line.Select(c => int.Parse(c.ToString())).ToList();
+                energyGrid.AddRow(energyLevelsRow);
+            }
+
+            var flashesAfter100Steps = 0;
+            var firstAllFlashStep = 0;
+            for (var step = 1; step <= 100 || firstAllFlashStep == 0; step++)
+            {
+                for (var rowIndex = 0; rowIndex < energyGrid.Rows; rowIndex++)
+                {
+                    for (var columnIndex = 0; columnIndex < energyGrid.Columns; columnIndex++)
+                    {
+                        energyGrid[rowIndex, columnIndex]++;
+                    }
+                }
+
+                if (energyGrid.ResetFlashEnergyLevels() == energyGrid.Rows * energyGrid.Columns &&
+                    firstAllFlashStep == 0)
+                {
+                    firstAllFlashStep = step;
+                }
+
+                if (step == 100)
+                {
+                    flashesAfter100Steps = energyGrid.FlashCount;
+                }
             }
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"{flashesAfter100Steps:N0} flashes after 100 steps\r\n" +
+                   $"First step during which all octopuses flash is Step {firstAllFlashStep:N0}\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
-
     }
 }
---
+++ Advent of Code/Year 2021/Day_11_EnergyGrid.cs
@@ -0,0 +1,88 @@
+﻿namespace Advent_of_Code.Year_2021_Day_11
+{
+    public class EnergyGrid
+    {
+        private int[,] _energyGrid;
+        private int _currentRowIndex = 0;
+        private int _flashCount = 0;
+
+        public EnergyGrid(int rows, int columns)
+        {
+            _energyGrid = new int[rows, columns];
+        }
+
+        public int Rows
+        {
+            get { return _energyGrid.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return _energyGrid.GetLength(1); }
+        }
+
+        public int this[int row, int column]
+        {
+            get => _energyGrid[row, column];
+            set
+            {
+                _energyGrid[row, column] = value;
+                if (value == 10)
+                {
+                    Flash(row, column);
+                }
+            }
+        }
+
+        public int FlashCount
+        {
+            get { return _flashCount; }
+        }
+
+        protected void Flash(int row, int column)
+        {
+            _flashCount++;
+            for (var rowIndex = row - 1; rowIndex <= row + 1; rowIndex++)
+            {
+                for (var columnIndex = column - 1; columnIndex <= column + 1; columnIndex++)
+                {
+                    if (rowIndex >= 0 && rowIndex < this.Rows &&
+                        columnIndex >= 0 && columnIndex < this.Columns &&
+                        (rowIndex != row || columnIndex != column))
+                    {
+                        this[rowIndex, columnIndex]++;
+                    }
+                }
+            }
+        }
+
+        public int ResetFlashEnergyLevels()
+        {
+            var currentFlashCount = 0;
+            for (var rowIndex = 0; rowIndex < this.Rows; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < this.Columns; columnIndex++)
+                {
+                    if (this[rowIndex, columnIndex] > 9)
+                    {
+                        this[rowIndex, columnIndex] = 0;
+                        currentFlashCount++;
+                    }
+                }
+            }
+            return currentFlashCount;
+        }
+
+        public void AddRow(List<int> energyLevelsInRow)
+        {
+            if (energyLevelsInRow.Count == this.Columns)
+            {
+                for (var columnIndex = 0; columnIndex < energyLevelsInRow.Count; columnIndex++)
+                {
+                    _energyGrid[_currentRowIndex, columnIndex] = energyLevelsInRow[columnIndex];
+                }
+                _currentRowIndex++;
+            }
+        }
+    }
+}
