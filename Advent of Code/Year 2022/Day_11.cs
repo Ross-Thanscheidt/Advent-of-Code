a10c727cce8a29e991ae8b3de616ddc72728baad
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-01 10:24:09 -0600

    Add Year 2022 Framework

---
+++ Advent of Code/Year 2022/Day_11.cs
@@ -0,0 +1,19 @@
+﻿namespace Advent_of_Code
+{
+    public partial class Year_2022 : IYear
+    {
+        public string Day_11(StringReader input)
+        {
+            var startTimestamp = DateTime.Now;
+
+            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            {
+            }
+
+            var endTimestamp = DateTime.Now;
+
+            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+        }
+
+    }
+}
