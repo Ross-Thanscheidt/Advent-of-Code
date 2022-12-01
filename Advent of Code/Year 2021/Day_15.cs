379eae2c10c44a988db80e037418d18a4eb9bc08
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-01 09:50:35 -0600

    Year 2021 Day 15 (work-in-progress)

--- Advent of Code/Year 2021/Day_15.cs
+++ Advent of Code/Year 2021/Day_15.cs
@@ -1,19 +1,105 @@
-﻿namespace Advent_of_Code
+﻿using Advent_of_Code.Year_2021_Day_15;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Advent_of_Code
 {
     public partial class Year_2021 : IYear
     {
+
+        private CavernMap? _cavernMap;
+        private int _lowestTotalRisk;
+
         public string Day_15(StringReader input)
         {
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            // Get input lines
+            var lines = new List<string>();
+            for (var line = input.ReadLine()?.Trim(); line != null; line = input.ReadLine())
             {
+                if (line.Length > 0)
+                {
+                    lines.Add(line);
+                }
             }
 
-            var endTimestamp = DateTime.Now;
+            // Load array of risk levels into CavernMap object
+            _cavernMap = new CavernMap(lines.Count, lines[0].Length);
+            foreach (var line in lines)
+            {
+                var riskLevelsRow = line.Select(c => int.Parse(c.ToString())).ToList();
+                _cavernMap.AddRow(riskLevelsRow);
+            }
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
-        }
+            _lowestTotalRisk = _cavernMap.MaxTotalRisk;
+            var startPosition = new Position { RowIndex = 0, ColumnIndex = 0 };
+            var potentialPaths = new PriorityQueue<(List<Position>, int), int>();
+            (List<Position>, int) startElement = new (new List<Position>() { startPosition }, (int)(_cavernMap?.Rows + _cavernMap?.Columns ?? 0));
+            potentialPaths.Enqueue(startElement, 0);
 
+            var keepLooking = true;
+            while (keepLooking)
+            {
+                keepLooking = potentialPaths.TryDequeue(out (List<Position>, int) currentElement, out int currentPriority);
+                if (keepLooking)
+                {
+                    var currentPath = currentElement.Item1;
+                    var currentTotalRisk = currentElement.Item2;
+                    var currentPosition = currentPath.Last();
+                    foreach (var nextPosition in (_cavernMap?[currentPosition.RowIndex, currentPosition.ColumnIndex].PossibleMoves)
+                                                .Where(nextPosition => !currentPath.Contains(nextPosition) &&
+                                                       currentTotalRisk + _cavernMap?[nextPosition].RiskLevel <= _lowestTotalRisk))
+                    {
+                        int nextPositionTotalRisk = currentTotalRisk + (int)(_cavernMap?[nextPosition].RiskLevel ?? 0);
+                        if (nextPosition.RowIndex == _cavernMap?.Rows - 1 &&
+                            nextPosition.ColumnIndex == _cavernMap?.Columns - 1)
+                        {
+                            if (nextPositionTotalRisk < _lowestTotalRisk)
+                            {
+                                _lowestTotalRisk = nextPositionTotalRisk;
+                            }
+                        }
+                        else
+                        {
+                            var nextPositionPath = currentPath.ToList();
+                            nextPositionPath.Add(nextPosition);
+                            potentialPaths.Enqueue((nextPositionPath, nextPositionTotalRisk), (int)(nextPositionTotalRisk + _cavernMap?.Rows - nextPosition.RowIndex + _cavernMap?.Columns - nextPosition.ColumnIndex));
+                        }
+                    }
+                    keepLooking = currentTotalRisk <= _lowestTotalRisk;
+                }
+            }
+                var endTimestamp = DateTime.Now;
+
+                return $"CavernMap is {_cavernMap.Columns}x{_cavernMap.Rows} with a Max Total Risk of {_cavernMap.MaxTotalRisk:N0}\r\n" +
+                       $"The lowest total risk of any path from the top left to the bottom right is {_lowestTotalRisk:N0}\r\n" +
+                       $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            }
+
+            //private void FindAllPaths(Position position, List<Position> currentPath, int currentTotalRisk)
+            //{
+            //if (position.RowIndex == _cavernMap?.Rows - 1 && position.ColumnIndex == _cavernMap?.Columns - 1)
+            //{
+            //    if (currentTotalRisk < _lowestTotalRisk)
+            //    {
+            //        _lowestTotalRisk = currentTotalRisk;
+            //    }
+            //}
+            //else
+            //{
+            //    foreach (var nextPosition in (_cavernMap?[position.RowIndex, position.ColumnIndex].PossibleMoves)
+            //                                    .Where(nextPosition => !currentPath.Contains(nextPosition) &&
+            //                                           currentTotalRisk + _cavernMap?[nextPosition].RiskLevel < _lowestTotalRisk &&
+            //                                           (_cavernMap?[nextPosition].LowestRiskLevelHere == 0 ||
+            //                                            currentTotalRisk + _cavernMap?[nextPosition].RiskLevel <= _cavernMap?[nextPosition].LowestRiskLevelHere)))
+            //    {
+            //        _cavernMap[nextPosition].LowestRiskLevelHere = currentTotalRisk + _cavernMap[nextPosition].RiskLevel;
+            //        currentPath.Add(nextPosition);
+            //        FindAllPaths(nextPosition, currentPath, currentTotalRisk + _cavernMap[nextPosition].RiskLevel);
+            //        currentPath.Remove(nextPosition);
+            //    }
+            //}
+            //}
     }
 }
---
+++ Advent of Code/Year 2021/Day_15_CavernMap.cs
@@ -0,0 +1,118 @@
+﻿namespace Advent_of_Code.Year_2021_Day_15
+{
+    public struct Position
+    {
+        public int RowIndex;
+        public int ColumnIndex;
+    }
+
+    public class CavernPosition
+    {
+        public int RiskLevel { get; set; }
+        public int LowestRiskLevelHere { get; set; }
+        public List<Position> PossibleMoves { get; set; }
+    }
+    public class CavernMap
+    {
+        private CavernPosition[,] _cavernMap;
+        private int _currentRowIndex = 0;
+        private int _maxTotalRisk = 0;
+
+        public CavernMap(int rows, int columns)
+        {
+            _cavernMap = new CavernPosition[rows, columns];
+        }
+
+        public int Rows
+        {
+            get { return _cavernMap.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return _cavernMap.GetLength(1); }
+        }
+
+        public int MaxTotalRisk
+        {
+            get { return _maxTotalRisk; }
+        }
+
+        public CavernPosition this[int row, int column]
+        {
+            get => _cavernMap[row, column];
+            set => _cavernMap[row, column] = value;
+        }
+
+        public CavernPosition this[Position position]
+        {
+            get => _cavernMap[position.RowIndex, position.ColumnIndex];
+            set => _cavernMap[position.RowIndex, position.ColumnIndex] = value;
+        }
+
+        public void AddRow(List<int> riskLevelsRow)
+        {
+            if (riskLevelsRow.Count == this.Columns)
+            {
+                for (var columnIndex = 0; columnIndex < riskLevelsRow.Count; columnIndex++)
+                {
+                    _cavernMap[_currentRowIndex, columnIndex] = new CavernPosition { RiskLevel = riskLevelsRow[columnIndex] };
+                }
+                _currentRowIndex++;
+
+                if (_currentRowIndex == this.Rows)
+                {
+                    InitializeMap();
+                }
+            }
+        }
+
+        private List<Position> PossibleMoves(int rowIndex, int columnIndex)
+        {
+            var moves = new List<Position>();
+
+            // Down
+            if (rowIndex < this.Rows - 1)
+            {
+                moves.Add(new Position { RowIndex = rowIndex + 1, ColumnIndex = columnIndex });
+            }
+
+            // Right
+            if (columnIndex < this.Columns - 1)
+            {
+                moves.Add(new Position { RowIndex = rowIndex, ColumnIndex = columnIndex + 1 });
+            }
+
+            // Left
+            if (columnIndex > 0)
+            {
+                moves.Add(new Position { RowIndex = rowIndex, ColumnIndex = columnIndex - 1 });
+            }
+
+            // Up - Moving up is not an option if on the right side if never occupying the same position twice
+            if (columnIndex < this.Columns - 1 && rowIndex > 0)
+            {
+                moves.Add(new Position { RowIndex = rowIndex - 1, ColumnIndex = columnIndex });
+            }
+
+            return moves;
+        }
+
+        private void InitializeMap()
+        {
+            // Initialize lowest total risk level here and define the list of possible moves for each cavern position
+            for (var rowIndex = 0; rowIndex < this.Rows; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < this.Columns; columnIndex++)
+                {
+                    _cavernMap[rowIndex, columnIndex].LowestRiskLevelHere = 0;
+                    _cavernMap[rowIndex, columnIndex].PossibleMoves = PossibleMoves(rowIndex, columnIndex);
+                }
+            }
+
+            // Calculate the Max Total Risk using a simple path (go down and then go right)
+            _maxTotalRisk = Enumerable.Range(1, this.Rows - 1).Sum(rowIndex => _cavernMap[rowIndex, 0].RiskLevel) +
+                            Enumerable.Range(1, this.Columns - 1).Sum(columnIndex => _cavernMap[this.Rows - 1, columnIndex].RiskLevel);
+        }
+    }
+}
