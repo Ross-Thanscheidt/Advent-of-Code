78938d2f9e7ce6d7c435da9d93a2e84ebf1a8358
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-02 10:32:52 -0600

    Year 2022 Day 2 Rock Paper Scissors

--- Advent of Code/Year 2022/Day_02.cs
+++ Advent of Code/Year 2022/Day_02.cs
@@ -4,16 +4,41 @@
     {
         public string Day_02(StringReader input)
         {
-            var startTimestamp = DateTime.Now;
+            var allLines = input.ReadToEnd().Split("\r\n");
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            var startTimestamp = DateTime.Now;
+            
+            Dictionary<char, string> objectLoseDrawWin = new()
             {
-            }
+                { 'X', "BAC" },
+                { 'Y', "CBA" },
+                { 'Z', "ACB" }
+            };
+
+            long objectScore = allLines
+                .Select(line => new { opponent = line[0], myObject = line[2] })
+                .Sum(line =>
+                        "XYZ".IndexOf(line.myObject) + 1 + // Rock=1, Paper=2, Scissors = 3
+                        objectLoseDrawWin[line.myObject].IndexOf(line.opponent) * 3); // Lose=0, Draw=3, Win=6
+
+            Dictionary<char, int[]> outcomeLoseDrawWin = new()
+            {
+                { 'A', new int[] { 3, 1, 2 } },
+                { 'B', new int[] { 1, 2, 3 } },
+                { 'C', new int[] { 2, 3, 1 } }
+            };
+
+            long outcomeScore = allLines
+                .Select(line => new { opponent = line[0], myOutcome = line[2] })
+                .Sum(line =>
+                    outcomeLoseDrawWin[line.opponent]["XYZ".IndexOf(line.myOutcome)] + // Rock=1, Paper=2, Scissors=3
+                    "XYZ".IndexOf(line.myOutcome) * 3); // Lose=0, Draw=3, Win=6
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"Score is {objectScore:N0} when X/Y/Z means object (Rock/Paper/Scissors)\r\n" +
+                   $"Score is {outcomeScore:N0} when X/Y/Z means outcome (Lose/Draw/Win)\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
-
     }
 }
