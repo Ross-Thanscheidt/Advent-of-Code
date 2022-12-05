114e2719247ea2341ad128f222ff77ab588aaaef
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-05 16:49:40 -0600

    Year 2022 Day 4 Camp Cleanup

--- Advent of Code/Year 2022/Day_04.cs
+++ Advent of Code/Year 2022/Day_04.cs
@@ -4,15 +4,37 @@
     {
         public string Day_04(StringReader input)
         {
+            var lines = input.ReadToEnd().Split("\r\n");
+
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
-            {
-            }
+            var rangePairs = lines
+                .Select(line =>
+                {
+                    var range1 = line.Split(",")[0];
+                    var range2 = line.Split(",")[1];
+                    return new
+                    {
+                        range1 = new { Start = int.Parse(range1.Split("-")[0]), End = int.Parse(range1.Split("-")[1]) },
+                        range2 = new { Start = int.Parse(range2.Split("-")[0]), End = int.Parse(range2.Split("-")[1]) }
+                    };
+                });
 
+            var containingPairs = rangePairs
+                .Where(pair =>
+                    (pair.range1.Start >= pair.range2.Start && pair.range1.End <= pair.range2.End) ||
+                    (pair.range2.Start >= pair.range1.Start && pair.range2.End <= pair.range1.End));
+
+            var overlappingPairs = rangePairs
+                .Where(pair =>
+                    !((pair.range1.Start < pair.range2.Start && pair.range1.End < pair.range2.Start) ||
+                      (pair.range1.Start > pair.range2.End && pair.range1.End > pair.range2.End)));
+            
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"There are {containingPairs.Count():N0} assignment pairs where one range fully contains the other\r\n" +
+                   $"There are {overlappingPairs.Count():N0} assignment pairs where the ranges overlap\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
 
     }
