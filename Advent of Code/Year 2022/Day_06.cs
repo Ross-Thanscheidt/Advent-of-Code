82272a722692e176f175123de4cb13d0706d78c5
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-07 09:36:09 -0600

    Year 2022 Day 6 Tuning Trouble

--- Advent of Code/Year 2022/Day_06.cs
+++ Advent of Code/Year 2022/Day_06.cs
@@ -4,16 +4,29 @@
     {
         public string Day_06(StringReader input)
         {
+            var line = input.ReadToEnd();
+
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            var distinctCharacterLengths = new[] { 4, 14 };
+            var charsProcessed = new List<int>();
+            
+            foreach (var chunkSize in distinctCharacterLengths)
             {
+                var idx = 0;
+                while (idx + chunkSize <= line.Length &&
+                       line.AsSpan(idx, chunkSize).ToArray().Distinct().Count() != chunkSize)
+                {
+                    idx++;
+                }
+                charsProcessed.Add(idx + chunkSize);
             }
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"{charsProcessed[0]:N0} chars processed to reach the first start-of-packet marker ({line.Substring(charsProcessed[0] - distinctCharacterLengths[0], distinctCharacterLengths[0])})\r\n" +
+                   $"{charsProcessed[1]:N0} chars processed to reach the first start-of-message marker ({line.Substring(charsProcessed[1] - distinctCharacterLengths[1], distinctCharacterLengths[1])})\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
-
     }
 }
