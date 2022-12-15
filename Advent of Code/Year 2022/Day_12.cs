95ac7d0c0946f48d62b5117429875d1978ef8432
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2022-12-15 10:35:08 -0600

    Year 2022 Day 12 Hill Climbing Algorithm

--- Advent of Code/Year 2022/Day_12.cs
+++ Advent of Code/Year 2022/Day_12.cs
@@ -1,4 +1,6 @@
-﻿namespace Advent_of_Code
+﻿using Advent_of_Code.Year_2022_Day_12;
+
+namespace Advent_of_Code
 {
     public partial class Year_2022 : IYear
     {
@@ -6,13 +8,87 @@
         {
             var startTimestamp = DateTime.Now;
 
-            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            // Get input lines
+            var lines = new List<string>();
+            for (var line = input.ReadLine()?.Trim(); line != null; line = input.ReadLine())
             {
+                if (line.Length > 0)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            var originalStartLeastSteps = 0;
+            var bestStartLeastSteps = 0;
+
+            Position? originalStartPosition = null;
+            var startingPositions = new Stack<Position>();
+
+            while (originalStartPosition == null || startingPositions.Any())
+            {
+                // Load array of elevation levels into AreaMap object
+                var areaMap = new AreaMap(lines.Count, lines[0].Length);
+                foreach (var line in lines)
+                {
+                    areaMap.AddRow(line);
+                }
+
+                // Set these values after the areaMap is built the first time
+                if (originalStartPosition == null)
+                {
+                    originalStartPosition = areaMap.StartPosition;
+                    bestStartLeastSteps = areaMap.Rows * areaMap.Columns;
+                    foreach (var position in areaMap.StartingPositions)
+                    {
+                        startingPositions.Push(position);
+                    }
+                }
+
+                // Find the fewest steps from the next starting position
+
+                var startingPosition = startingPositions.Pop();
+                areaMap.StartPosition = startingPosition;
+
+                var potentialMoves = new PriorityQueue<(Position Position, int Steps), int>();
+
+                // First move is from the current starting position
+                potentialMoves.Enqueue(new(areaMap.StartPosition, 0), 0);
+                areaMap[areaMap.StartPosition].LeastStepsHere = 0;
+
+                // Evaluate each remaining potential move
+                while (potentialMoves.TryDequeue(out (Position Position, int Steps) currentMove, out int currentPriority))
+                {
+                    foreach (var nextPosition in areaMap[currentMove.Position].PossibleMoves
+                        .Where(nextPosition =>
+                            currentMove.Steps + 1 < areaMap[nextPosition].LeastStepsHere &&
+                            currentMove.Steps + 1 < areaMap[areaMap.EndPosition].LeastStepsHere))
+                    {
+                        areaMap[nextPosition].LeastStepsHere = currentMove.Steps + 1;
+
+                        if (nextPosition != areaMap.EndPosition)
+                        {
+                            var priority =
+                                Math.Abs(areaMap.EndPosition.RowIndex - nextPosition.RowIndex) +
+                                Math.Abs(areaMap.EndPosition.ColumnIndex - nextPosition.ColumnIndex) +
+                                (areaMap[nextPosition].ElevationLevel - areaMap[currentMove.Position].ElevationLevel);
+                            potentialMoves.Enqueue((nextPosition, currentMove.Steps + 1), priority);
+                        }
+                    }
+                }
+
+                if (startingPosition == originalStartPosition)
+                {
+                    originalStartLeastSteps = areaMap[areaMap.EndPosition].LeastStepsHere;
+                }
+
+                bestStartLeastSteps = Math.Min(bestStartLeastSteps, areaMap[areaMap.EndPosition].LeastStepsHere);
             }
 
             var endTimestamp = DateTime.Now;
 
-            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+            return $"{originalStartLeastSteps:N0} are the fewest steps required from the designated Start position\r\n" +
+                   $"{bestStartLeastSteps:N0} are the fewest steps required from any position with the lowest elevation\r\n" +
+                   $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
         }
 
     }
---
+++ Advent of Code/Year 2022/Day_12_AreaMap.cs
@@ -0,0 +1,165 @@
+﻿namespace Advent_of_Code.Year_2022_Day_12
+{
+    public struct Position
+    {
+        public int RowIndex;
+
+        public int ColumnIndex;
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return left.RowIndex == right.RowIndex && left.ColumnIndex == right.ColumnIndex;
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+
+    public class AreaPosition
+    {
+        public char ElevationLevel { get; set; }
+
+        public int LeastStepsHere { get; set; }
+
+        public List<Position> PossibleMoves { get; set; } = new();
+    }
+
+    public class AreaMap
+    {
+        private AreaPosition[,] _areaMap;
+        private Position _endPosition;
+        private int _currentRowIndex = 0;
+
+        public AreaMap(int rows, int columns)
+        {
+            _areaMap = new AreaPosition[rows, columns];
+        }
+
+        public int Rows
+        {
+            get { return _areaMap.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return _areaMap.GetLength(1); }
+        }
+
+        public Position StartPosition { get; set; }
+
+        public Position EndPosition => _endPosition;
+
+        public AreaPosition this[int row, int column]
+        {
+            get => _areaMap[row, column];
+            set => _areaMap[row, column] = value;
+        }
+
+        public AreaPosition this[Position position]
+        {
+            get => _areaMap[position.RowIndex, position.ColumnIndex];
+            set => _areaMap[position.RowIndex, position.ColumnIndex] = value;
+        }
+
+        public void AddRow(string line)
+        {
+            var elevationLevelsRow = line.ToList();
+            if (elevationLevelsRow.Count == this.Columns)
+            {
+                for (var columnIndex = 0; columnIndex < elevationLevelsRow.Count; columnIndex++)
+                {
+                    var elevationLevel = elevationLevelsRow[columnIndex];
+                    if (elevationLevel == 'S')
+                    {
+                        StartPosition = new Position { RowIndex = _currentRowIndex, ColumnIndex = columnIndex }; 
+                        elevationLevel = 'a';
+                    }
+                    else if (elevationLevel == 'E')
+                    {
+                        _endPosition.RowIndex = _currentRowIndex;
+                        _endPosition.ColumnIndex = columnIndex;
+                        elevationLevel = 'z';
+                    }
+                    _areaMap[_currentRowIndex, columnIndex] = new AreaPosition { ElevationLevel = elevationLevel };
+                }
+                _currentRowIndex++;
+
+                if (_currentRowIndex == this.Rows)
+                {
+                    InitializeMap();
+                }
+            }
+        }
+
+        private List<Position> PossibleMoves(int rowIndex, int columnIndex)
+        {
+            var maxElevationLevel = _areaMap[rowIndex, columnIndex].ElevationLevel + 1;
+
+            var moves = new List<Position>();
+
+            // Down
+            if (rowIndex < this.Rows - 1 &&
+                _areaMap[rowIndex + 1, columnIndex].ElevationLevel <= maxElevationLevel)
+            {
+                moves.Add(new Position { RowIndex = rowIndex + 1, ColumnIndex = columnIndex });
+            }
+
+            // Right
+            if (columnIndex < this.Columns - 1 &&
+                _areaMap[rowIndex, columnIndex + 1].ElevationLevel <= maxElevationLevel)
+            {
+                moves.Add(new Position { RowIndex = rowIndex, ColumnIndex = columnIndex + 1 });
+            }
+
+            // Left
+            if (columnIndex > 0 &&
+                _areaMap[rowIndex, columnIndex - 1].ElevationLevel <= maxElevationLevel)
+            {
+                moves.Add(new Position { RowIndex = rowIndex, ColumnIndex = columnIndex - 1 });
+            }
+
+            // Up
+            if (rowIndex > 0 &&
+                _areaMap[rowIndex - 1, columnIndex].ElevationLevel <= maxElevationLevel)
+            {
+                moves.Add(new Position { RowIndex = rowIndex - 1, ColumnIndex = columnIndex });
+            }
+
+            return moves;
+        }
+
+        private void InitializeMap()
+        {
+            // Initialize lowest total risk level here and define the list of possible moves for each cavern position
+            for (var rowIndex = 0; rowIndex < this.Rows; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < this.Columns; columnIndex++)
+                {
+                    _areaMap[rowIndex, columnIndex].LeastStepsHere = this.Rows * this.Columns;
+                    _areaMap[rowIndex, columnIndex].PossibleMoves = PossibleMoves(rowIndex, columnIndex);
+                }
+            }
+        }
+
+        public IEnumerable<Position> StartingPositions
+        {
+            get
+            {
+                var startingPositions = new List<Position>();
+                foreach (var rowIndex in Enumerable.Range(0, this.Rows))
+                {
+                    foreach (var columnIndex in Enumerable.Range(0, this.Columns))
+                    {
+                        if (_areaMap[rowIndex, columnIndex].ElevationLevel == 'a')
+                        {
+                            startingPositions.Add(new Position() { RowIndex = rowIndex, ColumnIndex = columnIndex });
+                        }
+                    }
+                }
+                return startingPositions;
+            }
+        }
+    }
+}
