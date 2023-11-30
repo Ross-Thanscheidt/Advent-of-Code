21265b264c07faca06b9763d061b9ccf9fb1a411
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-11-30 15:48:23 -0600

    Add Year 2023 Structure

---
+++ Advent of Code/Year 2023/Day_19.cs
@@ -0,0 +1,19 @@
+﻿namespace Advent_of_Code
+{
+    public partial class Year_2023 : IYear
+    {
+        public string Day_19(StringReader input)
+        {
+            var startTimestamp = DateTime.Now;
+
+            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            {
+            }
+
+            var endTimestamp = DateTime.Now;
+
+            return $"({(endTimestamp - startTimestamp) * 1000:s\\.ffffff} ms)";
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code
 My solutions for the [Advent of Code](https://AdventOfCode.com) challenges.
 
-I'm using WinForms, C#, and .NET 7.
+I'm using WinForms, C#, and .NET 8.
