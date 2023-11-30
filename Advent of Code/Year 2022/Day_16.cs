f72e52e80536469fd5182017e8422cc133cb84d6
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-11-30 14:47:22 -0600

    Year 2022 Day 16 (work in progress)

--- Advent of Code/Year 2022/Day_16.cs
+++ Advent of Code/Year 2022/Day_16.cs
@@ -1,4 +1,6 @@
-﻿namespace Advent_of_Code
+﻿using System.Diagnostics;
+
+namespace Advent_of_Code
 {
     public partial class Year_2022 : IYear
     {
@@ -8,6 +10,13 @@
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                if (line.StartsWith("Valve "))
+                {
+                    var valve = line.Split(" ")[1];
+                    var flowRate = int.Parse(line.Split("=")[1].Split(";")[0]);
+                    var neighbors = line.Split("valve")[1].Split(",").Select(v => v.Split(" ")[v.Split(" ").Length-1]);
+                    Debug.WriteLine($"Valve {valve} has a flow rate of {flowRate} and connects to {string.Join(", ", neighbors)}");
+                }
             }
 
             var endTimestamp = DateTime.Now;
