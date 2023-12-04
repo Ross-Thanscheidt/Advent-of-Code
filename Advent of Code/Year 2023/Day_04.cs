a2993f3980d2bd182b9a144de762432302bd09ef
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-04 00:26:46 -0600

    Year 2023 Day 4 Scratchcards

--- Advent of Code/Year 2023/Day_04.cs
+++ Advent of Code/Year 2023/Day_04.cs
@@ -1,20 +1,76 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Advent_of_Code
 {
     public partial class Year_2023 : IYear
     {
+        [GeneratedRegex("(.*?(?<Number>\\d+).*?)*")]
+        private static partial Regex Day_04_CardNumbersRegex();
+
         public string Day_04(StringReader input)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            int pointSum = 0;
+            int scratchcardCount = 0;
+
+            var cardsWon = new List<int>();
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                var winningMatchGroups = Day_04_CardNumbersRegex().Match(line.Split(":")[1].Split("|")[0]).Groups;
+                var winningNumbers = winningMatchGroups["Number"].Captures
+                    .Select(capture => int.Parse(capture.Value))
+                    .ToList();
+
+                var possessedMatchGroups = Day_04_CardNumbersRegex().Match(line.Split(":")[1].Split("|")[1]).Groups;
+                var possessedNumbers = possessedMatchGroups["Number"].Captures
+                    .Select(capture => int.Parse(capture.Value))
+                    .ToList();
+
+                var matchedNumbers = winningNumbers
+                    .Join(
+                        possessedNumbers,
+                        winningNumber => winningNumber,
+                        possessedNumber => possessedNumber,
+                        (winningNumber, possessedNumber) => possessedNumber);
+
+                int cardCopies = 1;
+
+                if (cardsWon.Count > 0)
+                {
+                    cardCopies += cardsWon[0];
+                    cardsWon.RemoveAt(0);
+                }
+
+                scratchcardCount += cardCopies;
+
+                var numbersMatched = matchedNumbers.Count();
+
+                if (numbersMatched > 0)
+                {
+                    pointSum += 1 << (numbersMatched - 1);
+
+                    for (int cardIndex = 0; cardIndex < numbersMatched; cardIndex++)
+                    {
+                        if (cardIndex < cardsWon.Count)
+                        {
+                            cardsWon[cardIndex] += cardCopies;
+                        }
+                        else
+                        {
+                            cardsWon.Add(cardCopies);
+                        }
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{pointSum:N0} points in total\r\n" +
+                   $"{scratchcardCount:N0} total scratchcards\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
 
     }
