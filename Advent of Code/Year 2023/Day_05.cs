bb3ee0f0eff8984ec05016b27aeee52f00f82841
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-05 00:30:43 -0600

    Year 2023 Day 5 If You Give A Seed A Fertilizer Part One

--- Advent of Code/Year 2023/Day_05.cs
+++ Advent of Code/Year 2023/Day_05.cs
@@ -8,13 +8,74 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long lowestLocationNumber = 0;
+
+            var debug = "";
+            List<long> seeds = [];
+            string sourceName = "";
+            string destinationName = "";
+            List<(string Source, string Destination, long DestRangeStart, long SourceRangeStart, long RangeLength)> map = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                if (line.Length > 0)
+                {
+                    if (line.StartsWith("seeds: "))
+                    {
+                        seeds = line["seeds: ".Length..]
+                            .Split(" ")
+                            .Select(s => long.Parse(s))
+                            .ToList();
+                    }
+                    else if (line.Contains("map:"))
+                    {
+                        sourceName = line.Split(" ")[0].Split("-to-")[0];
+                        destinationName = line.Split(" ")[0].Split("-to-")[1];
+                    }
+                    else if (Char.IsDigit(line[0]))
+                    {
+                        var numbers = line.Split(" ").Select(n => long.Parse(n)).ToList();
+                        map.Add((sourceName, destinationName, numbers[0], numbers[1], numbers[2]));
+                    }
+                }
+            }
+
+            foreach (var seed in seeds)
+            {
+                sourceName = "seed";
+                var sourceNumber = seed;
+
+                while (sourceName != "location")
+                {
+                    var range = map
+                        .Where(
+                            m => m.Source == sourceName &&
+                            sourceNumber >= m.SourceRangeStart &&
+                            sourceNumber <= m.SourceRangeStart + m.RangeLength);
+
+                    if (range.Any())
+                    {
+                        var (Source, Destination, DestRangeStart, SourceRangeStart, RangeLength) = range.First();
+                        sourceName = Destination;
+                        sourceNumber = DestRangeStart + sourceNumber - SourceRangeStart;
+                    }
+                    else
+                    {
+                        sourceName = map.First(m => m.Source == sourceName).Destination;
+                    }
+                }
+
+                if (lowestLocationNumber == 0 || sourceNumber < lowestLocationNumber)
+                {
+                    lowestLocationNumber = sourceNumber;
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{debug}\r\n" +
+                   $"{lowestLocationNumber:N0} is the lowest location number that corresponds to any of the initial seed numbers\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
 
     }
