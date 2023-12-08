e673998bc96181455aa90763b18c992228ea3dee
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-08 01:30:07 -0600

    Year 2023 Day 7 Camel Cards Part One completed

--- Advent of Code/Year 2023/Day_07.cs
+++ Advent of Code/Year 2023/Day_07.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Advent_of_Code.Year_2023_Day_07;
 
 namespace Advent_of_Code
 {
@@ -8,14 +9,62 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            var totalWinnings = 0;
+
+            List<Hand> hands = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                var hand = new Hand
+                {
+                    Cards = line.Split(" ")[0],
+                    Bid = int.Parse(line.Split(" ")[1]),
+                    HandType = 0,
+                    ComparableCards = ""
+                };
+
+                foreach (char card in hand.Cards)
+                {
+                    hand.ComparableCards += (char)("23456789TJQKA".IndexOf(card) + 'A');
+                }
+
+                var handType = string.Join("",
+                    hand.ComparableCards
+                        .GroupBy(
+                            card => card,
+                            (card, cards) => new { Card = card, Count = cards.Count() })
+                        .OrderByDescending(c => c.Count)
+                        .Select(c => c.Count.ToString()));
+
+                hand.HandType = handType switch
+                    {
+                        "5" => 7,
+                        "41" => 6,
+                        "32" => 5,
+                        "311" => 4,
+                        "221" => 3,
+                        "2111" => 2,
+                        "11111" => 1,
+                        _ => 0
+                    };
+
+                hands.Add(hand);
+            }
+
+            var rankedHands = hands
+                .OrderBy(hand => hand.HandType)
+                .ThenBy(hand => hand.ComparableCards)
+                .ToList();
+
+            for (var rank = 1; rank <= rankedHands.Count; rank++)
+            {
+                totalWinnings += rank * rankedHands[rank - 1].Bid;
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{totalWinnings:N0} are the total winnings\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
---
+++ Advent of Code/Year 2023/Day_07_Hand.cs
@@ -0,0 +1,10 @@
+﻿namespace Advent_of_Code.Year_2023_Day_07
+{
+    public struct Hand
+    {
+        public string Cards;
+        public int Bid;
+        public int HandType;
+        public string ComparableCards;
+    }
+}
