9f992c3558603bef40c93f7e8000dca8e63d46eb
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-10 17:44:13 -0600

    Year 2023 Day 9 Mirage Maintenance

--- Advent of Code/Year 2023/Day_09.cs
+++ Advent of Code/Year 2023/Day_09.cs
@@ -8,13 +8,36 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long extrapolatedSum = 0;
+            long extrapolatedSumBackwards = 0;
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                var values = line.Split(" ").Select(s => long.Parse(s)).ToList();
+
+                int alternateSubtractAndAddForBackwards = 1;
+                int lastValueIndex = values.Count - 1;
+                while (values.Any(v => v != 0))
+                {
+                    extrapolatedSum += values[lastValueIndex];
+
+                    extrapolatedSumBackwards += alternateSubtractAndAddForBackwards * values[0];
+                    alternateSubtractAndAddForBackwards *= -1;
+
+                    for (int valuesIndex = 0; valuesIndex < lastValueIndex; valuesIndex++)
+                    {
+                        values[valuesIndex] = values[valuesIndex+1] - values[valuesIndex];
+                    }
+
+                    values[lastValueIndex--] = 0;
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{extrapolatedSum:N0} is the sum of the extrapolated values\r\n" +
+                   $"{extrapolatedSumBackwards:N0} is the sum of the extrapolated values going backwards\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
 
     }
