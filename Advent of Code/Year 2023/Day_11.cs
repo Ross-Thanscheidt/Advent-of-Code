5f46fdb4c07c6734e7315e65e3b69eda96cb3546
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-12-15 10:58:01 -0600

    Year 2023 Day 11 Cosmic Expansion Refactor to facilitate multiple expansion factors

--- Advent of Code/Year 2023/Day_11.cs
+++ Advent of Code/Year 2023/Day_11.cs
@@ -6,97 +6,116 @@ namespace Advent_of_Code
 {
     public partial class Year_2023 : IYear
     {
+        [GeneratedRegex("#")]
+        private static partial Regex Day_11_GalaxyRegex();
+
         public string Day_11(StringReader input)
         {
-            const long EXPAND_FACTOR = 2;
-            const long EXPAND_FACTOR_OLD = 1_000_000;
+            long[] EXPAND_FACTOR = [2];
+
+            long[] EXPAND_FACTOR_OLD_TEST = [10, 100];
+            long[] EXPAND_FACTOR_OLD = [1_000_000];
 
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long totalSteps = 0;
-            long totalStepsOld = 0;
+            Dictionary<long, long> totalSteps = [];
+            Dictionary<long, long> totalStepsOld = [];
 
             List<string> map = [];
             List<Position> galaxies = [];
-            List<Position> galaxiesOld = [];
-
-            long currentRow = 0;
-            long currentRowOld = 0;
+            int currentRow = 0;
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
                 map.Add(line);
                 if (line.Contains('#'))
                 {
-                    var galaxyColumns = Regex.Matches(line, "#").Cast<Match>().Select(m => m.Index).ToList();
+                    var galaxyColumns = Day_11_GalaxyRegex().Matches(line).Cast<Match>().Select(m => m.Index).ToList();
                     foreach (var galaxyColumn in galaxyColumns)
                     {
                         galaxies.Add(new(galaxyColumn, currentRow));
-                        galaxiesOld.Add(new(galaxyColumn, currentRowOld));
                     }
-                }
-                else
-                {
-                    // Expand rows without galaxies
-                    currentRow += EXPAND_FACTOR - 1;
-                    currentRowOld += EXPAND_FACTOR_OLD - 1;
                 }
 
                 currentRow++;
-                currentRowOld++;
             }
 
-            // Expand columns without galaxies
-            long currentColumn = 0;
-            long currentColumnOld = 0;
+            bool testInput = map.Count <= 10;
 
-            for (var columnIndex = 0; columnIndex < map[0].Length; columnIndex++)
+            for (var part = 1; part <= 2; part++)
             {
-                if (!map.Any(line => line[columnIndex] == '#'))
+                foreach (var expandFactor in part == 1 ? EXPAND_FACTOR : testInput ? EXPAND_FACTOR_OLD_TEST : EXPAND_FACTOR_OLD)
                 {
-                    // Expand this column by adding 1 to X of each galaxy to the right of the current column
-                    galaxies.ForEach(position => position.X += position.X > currentColumn ? EXPAND_FACTOR - 1 : 0);
-                    currentColumn += EXPAND_FACTOR - 1;
+                    List<Position> expandedGalaxies = galaxies.ConvertAll(galaxy => new Position(galaxy.X, galaxy.Y));
 
-                    // Expand this column by adding 1,000,000 to X of each galaxy to the right of the current column
-                    galaxiesOld.ForEach(position => position.X += position.X > currentColumnOld ? EXPAND_FACTOR_OLD - 1 : 0);
-                    currentColumnOld += EXPAND_FACTOR_OLD - 1;
-                }
+                    // Expand rows that do not have galaxies
+                    long currentRowExpanded = 0;
 
-                currentColumn++;
-                currentColumnOld++;
-            }
+                    for (currentRow = 0; currentRow < map.Count; currentRow++)
+                    {
+                        if (!map[currentRow].Contains('#'))
+                        {
+                            // Expand this row by increasing Y of each galaxy below this row
+                            expandedGalaxies.ForEach(position => position.Y += position.Y > currentRowExpanded ? expandFactor - 1 : 0);
+                            currentRowExpanded += expandFactor - 1;
+                        }
 
-            // Count steps for Part One galaxies (each empty row/column expands by a factor of two)
-            for (var index1 = 0; index1 < galaxies.Count - 1; index1++)
-            {
-                for (var index2 = index1 + 1; index2 < galaxies.Count; index2++)
-                {
-                    var galaxy1 = galaxies[index1];
-                    var galaxy2 = galaxies[index2];
+                        currentRowExpanded++;
+                    }
 
-                    totalSteps += Math.Abs(galaxy2.X - galaxy1.X) + Math.Abs(galaxy2.Y - galaxy1.Y);
-                }
-            }
+                    // Expand columns that do not have galaxies
+                    long currentColumnExpanded = 0;
 
-            // Count steps for Part Two galaxies (each empty row/column expands by a factor of 1 million)
-            for (var index1 = 0; index1 < galaxiesOld.Count - 1; index1++)
-            {
-                for (var index2 = index1 + 1; index2 < galaxiesOld.Count; index2++)
-                {
-                    var galaxy1 = galaxiesOld[index1];
-                    var galaxy2 = galaxiesOld[index2];
+                    for (var currentColumn = 0; currentColumn < map[0].Length; currentColumn++)
+                    {
+                        if (!map.Any(line => line[currentColumn] == '#'))
+                        {
+                            // Expand this column by increasing X of each galaxy to the right of the current column
+                            expandedGalaxies.ForEach(position => position.X += position.X > currentColumnExpanded ? expandFactor - 1 : 0);
+                            currentColumnExpanded += expandFactor - 1;
+                        }
 
-                    totalStepsOld += Math.Abs(galaxy2.X - galaxy1.X) + Math.Abs(galaxy2.Y - galaxy1.Y);
+                        currentColumnExpanded++;
+                    }
+
+                    // Count steps between each pair of expanded galaxies
+                    for (var index1 = 0; index1 < expandedGalaxies.Count - 1; index1++)
+                    {
+                        for (var index2 = index1 + 1; index2 < expandedGalaxies.Count; index2++)
+                        {
+                            var galaxy1 = expandedGalaxies[index1];
+                            var galaxy2 = expandedGalaxies[index2];
+
+                            var steps = Math.Abs(galaxy2.X - galaxy1.X) + Math.Abs(galaxy2.Y - galaxy1.Y);
+
+                            if (part == 1)
+                            {
+                                if (!totalSteps.ContainsKey(expandFactor))
+                                {
+                                    totalSteps.Add(expandFactor, 0);
+                                }
+
+                                totalSteps[expandFactor] += steps;
+                            }
+                            else
+                            {
+                                if (!totalStepsOld.ContainsKey(expandFactor))
+                                {
+                                    totalStepsOld.Add(expandFactor, 0);
+                                }
+
+                                totalStepsOld[expandFactor] += steps;
+                            }
+                        }
+                    }
                 }
             }
 
             stopwatch.Stop();
 
-            return $"{totalSteps:N0} is the sum of the shortest lengths between every pair of galaxies\r\n" +
-                   $"{totalStepsOld:N0} is the sum of the shortest lengths between every pair of old galaxies\r\n" +
+            return $"{string.Join("\r\n", totalSteps.Select(kv => $"{kv.Value:N0} is the sum of the shortest lengths between every pair of galaxies (expanded by {kv.Key:N0})"))}\r\n" +
+                   $"{string.Join("\r\n", totalStepsOld.Select(kv => $"{kv.Value:N0} is the sum of the shortest lengths between every pair of old galaxies (expanded by {kv.Key:N0})"))}\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
