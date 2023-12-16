77a65cf580720b56f0da5c19ae8c912f41b71756
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-16 02:26:46 -0600

    Year 2023 Day 15 Lens Library

--- Advent of Code/Year 2023/Day_15.cs
+++ Advent of Code/Year 2023/Day_15.cs
@@ -1,21 +1,98 @@
 ﻿using System.Diagnostics;
+using Advent_of_Code.Year_2023_Day_15;
 
 namespace Advent_of_Code
 {
     public partial class Year_2023 : IYear
     {
+        private int Day_15_Hash(string stringToHash)
+        {
+            int currentValue = 0;
+
+            foreach (var c in stringToHash)
+            {
+                currentValue += c;
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+
+            return currentValue;
+        }
+
         public string Day_15(StringReader input)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long resultsSum = 0;
+            long focusingPower = 0;
+
+            Dictionary<int, List<Lens>> boxes = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                var steps = line.Split(",").ToList();
+
+                foreach (var step in steps)
+                {
+                    resultsSum += Day_15_Hash(step);
+
+
+                    var label = step.Split([.. "=-"])[0];
+                    var box = Day_15_Hash(label);
+                    var operation = step.Contains('=') ? '=' : '-';
+
+                    var lens = boxes.TryGetValue(box, out List<Lens>? value) ? value.Find(lens => lens.Label == label) : null;
+
+                    if (operation == '-')
+                    {
+                        if (lens != null)
+                        {
+                            boxes[box].Remove(lens);
+                        }
+                    }
+                    else if (operation == '=')
+                    {
+                        var focalLength = int.Parse(step.Split([.. "="])[1]);
+
+                        if (lens == null)
+                        {
+                            lens = new()
+                            {
+                                Label = label,
+                                FocalLength = focalLength
+                            };
+
+                            if (boxes.TryGetValue(box, out List<Lens>? boxElement))
+                            {
+                                boxElement.Add(lens);
+                            }
+                            else
+                            {
+                                boxes.Add(box, [lens]);
+                            }
+                        }
+                        else
+                        {
+                            lens.FocalLength = focalLength;
+                        }
+                    }
+                }
+            }
+
+            foreach (var box in boxes)
+            {
+                for (var slotIndex = 0; slotIndex < box.Value.Count; slotIndex++)
+                {
+                    var focusingPowerLens = (box.Key + 1) * (slotIndex + 1) * box.Value[slotIndex].FocalLength;
+                    focusingPower += focusingPowerLens;
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{resultsSum:N0} is the sum of the results\r\n" +
+                   $"{focusingPower:N0} is the focusing power of the resulting lens configuration\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
---
+++ Advent of Code/Year 2023/Day_15_Lens.cs
@@ -0,0 +1,8 @@
+﻿namespace Advent_of_Code.Year_2023_Day_15
+{
+    public class Lens
+    {
+        public string Label;
+        public int FocalLength;
+    }
+}
