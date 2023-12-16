16ca570bbb190a715f734572739569fbe447dd9f
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-12-15 18:31:41 -0600

    Year 2023 Day 14 Parabolic Reflector Dish Part One completed

--- Advent of Code/Year 2023/Day_14.cs
+++ Advent of Code/Year 2023/Day_14.cs
@@ -8,13 +8,39 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long totalLoad = 0;
+
+            List<List<char>> map = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                map.Add([.. line.ToCharArray()]);
+            }
+
+            for (var columnIndex = 0; columnIndex < map[0].Count; columnIndex++)
+            {
+                for (var rowIndex = 0; rowIndex < map.Count; rowIndex++)
+                {
+                    if (map[rowIndex][columnIndex] == 'O')
+                    {
+                        var targetRowIndex = rowIndex - 1;
+
+                        while (targetRowIndex >= 0 && map[targetRowIndex][columnIndex] == '.')
+                        {
+                            map[targetRowIndex][columnIndex] = 'O';
+                            map[targetRowIndex+1][columnIndex] = '.';
+                            targetRowIndex--;
+                        }
+
+                        totalLoad += map.Count - (targetRowIndex + 1);
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{totalLoad:N0} is the total load on the north support beams\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
 
     }
