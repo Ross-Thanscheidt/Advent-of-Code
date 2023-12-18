22a3879627c3fdf200847f3d1b36004e0b393dda
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2023-12-18 08:09:51 -0600

    Year 2023 Day 14 Parabolic Reflector Dish Working on Part Two

--- Advent of Code/Year 2023/Day_14.cs
+++ Advent of Code/Year 2023/Day_14.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Advent_of_Code.Year_2023_Day_14;
 
 namespace Advent_of_Code
 {
@@ -11,31 +12,75 @@ namespace Advent_of_Code
             long totalLoad = 0;
 
             List<List<char>> map = [];
+            List<Position> roundRocks = [];
+            List<Position> cubeRocks = [];
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
                 map.Add([.. line.ToCharArray()]);
             }
 
-            for (var columnIndex = 0; columnIndex < map[0].Count; columnIndex++)
+            var rows = map.Count;
+            var columns = map[0].Count;
+
+            for (var rowIndex = 0; rowIndex < rows; rowIndex++)
             {
-                for (var rowIndex = 0; rowIndex < map.Count; rowIndex++)
+                for (var columnIndex = 0; columnIndex < columns; columnIndex++)
                 {
-                    if (map[rowIndex][columnIndex] == 'O')
+                    var item = map[rowIndex][columnIndex];
+                    if (item == 'O')
                     {
-                        var targetRowIndex = rowIndex - 1;
-
-                        while (targetRowIndex >= 0 && map[targetRowIndex][columnIndex] == '.')
-                        {
-                            map[targetRowIndex][columnIndex] = 'O';
-                            map[targetRowIndex+1][columnIndex] = '.';
-                            targetRowIndex--;
-                        }
-
-                        totalLoad += map.Count - (targetRowIndex + 1);
+                        roundRocks.Add(new(columnIndex + 1, rows - rowIndex));
+                    }
+                    else if (item == '#')
+                    {
+                        cubeRocks.Add(new(columnIndex + 1, rows - rowIndex));
                     }
                 }
             }
+
+            Debug.WriteLine($"{string.Join(" ", roundRocks.Select(r => $"({r.X},{r.Y})"))}");
+            Debug.WriteLine($"{string.Join(" ", cubeRocks.Select(c => $"({c.X},{c.Y})"))}");
+
+            foreach (var column in roundRocks.Select(r => r.X).Distinct())
+            {
+                //roundRocks.Where(r => r.X == column)
+            }
+
+            roundRocks
+                .OrderByDescending(r => r.Y)
+                .ToList()
+                .ForEach(r => r.Y = Math.Max(
+                    r.Y,
+                    cubeRocks
+                        .Where(c => c.X == r.X && c.Y > r.Y)
+                        .OrderBy(c => c.Y)
+                        .Select(c => c.Y)
+                        .FirstOrDefault()));
+
+            Debug.WriteLine($"{string.Join(" ", roundRocks.Select(r => $"({r.X},{r.Y})"))}");
+
+            totalLoad = roundRocks.Select(r => r.Y).Sum();
+
+            //for (var columnIndex = 0; columnIndex < map[0].Count; columnIndex++)
+            //{
+            //    for (var rowIndex = 0; rowIndex < map.Count; rowIndex++)
+            //    {
+            //        if (map[rowIndex][columnIndex] == 'O')
+            //        {
+            //            var targetRowIndex = rowIndex - 1;
+
+            //            while (targetRowIndex >= 0 && map[targetRowIndex][columnIndex] == '.')
+            //            {
+            //                map[targetRowIndex][columnIndex] = 'O';
+            //                map[targetRowIndex+1][columnIndex] = '.';
+            //                targetRowIndex--;
+            //            }
+
+            //            totalLoad += map.Count - (targetRowIndex + 1);
+            //        }
+            //    }
+            //}
 
             stopwatch.Stop();
 
