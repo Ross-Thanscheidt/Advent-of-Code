6b0562e5753d1f21d0760ea48056660ed1abbc62
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-12-19 18:17:19 -0600

    Year 2023 Day 17 Clumsy Crucible Getting Started

--- Advent of Code/Year 2023/Day_17.cs
+++ Advent of Code/Year 2023/Day_17.cs
@@ -8,14 +8,25 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long leastHeatLoss = 0;
+
+            List<string> map = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                if (line.Length > 0)
+                {
+                    map.Add(line);
+                }
             }
+
+            var columns = map[0].Length;
+            var rows = map.Count;
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{leastHeatLoss:N0} is the least heat loss the crucible can incur\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
