be9e760b5b37e32ca95d8bd433aaf0183de1588b
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2024-11-30 18:34:44 -0600

    Add Year 2024 Structure

---
+++ Advent of Code/Year 2024/Day_03.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Advent_of_Code
+{
+    public partial class Year_2024 : IYear
+    {
+        public string Day_03(StringReader input)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (var line = input.ReadLine(); line != null; line = input.ReadLine())
+            {
+            }
+
+            stopwatch.Stop();
+
+            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+        }
+
+    }
+}
