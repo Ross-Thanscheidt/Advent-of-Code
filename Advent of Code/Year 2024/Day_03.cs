3d9343ada3d0de647625b3e09fb1dcaabc59ed86
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2024-12-03 00:11:38 -0600

    Year 2024 Day 3 Mull It Over

--- Advent of Code/Year 2024/Day_03.cs
+++ Advent of Code/Year 2024/Day_03.cs
@@ -1,21 +1,55 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Advent_of_Code
 {
     public partial class Year_2024 : IYear
     {
+        [GeneratedRegex("do\\(\\)|don't\\(\\)|mul\\((?<num1>\\d{1,3}),(?<num2>\\d{1,3})\\)")]
+        private static partial Regex Day_03_InstructionsRegex();
+
         public string Day_03(StringReader input)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long sumOfProducts = 0;
+            long sumOfEnabledProducts = 0;
+
+            var regex = Day_03_InstructionsRegex();
+            bool enabled = true;
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                sumOfProducts += regex.Matches(line).Cast<Match>()
+                    .Sum(match => match.Value.StartsWith("mul(")
+                        ? int.Parse(match.Groups["num1"].Value) * int.Parse(match.Groups["num2"].Value)
+                        : 0);
+
+                sumOfEnabledProducts += regex.Matches(line).Cast<Match>()
+                    .Sum(match =>
+                    {
+                        if (match.Value == "do()")
+                        {
+                            enabled = true;
+                            return 0;
+                        }
+                        else if (match.Value == "don't()")
+                        {
+                            enabled = false;
+                            return 0;
+                        }
+                        else
+                        {
+                            return enabled ? int.Parse(match.Groups["num1"].Value) * int.Parse(match.Groups["num2"].Value) : 0;
+                        }
+                    });
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{sumOfProducts:N0} is the sum of the results of the multiplications\r\n" +
+                   $"{sumOfEnabledProducts:N0} is the sum of the results of just the enabled multiplications\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
