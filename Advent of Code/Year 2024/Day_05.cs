29cd889b91b16048ecc13b59526f73a20c5f3855
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2024-12-05 00:23:00 -0600

    Year 2024 Day 5 Print Queue

--- Advent of Code/Year 2024/Day_05.cs
+++ Advent of Code/Year 2024/Day_05.cs
@@ -8,14 +8,52 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            long sumOfCorrectMiddlePageNumbers = 0;
+            long sumOfCorrectedMiddlePageNumbers = 0;
+
+            List<(int before, int after)> rules = [];
+
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                if (line.Contains('|'))
+                {
+                    List<int> pageNumbers = line.Split('|').Select(number => int.Parse(number)).ToList();
+                    rules.Add((pageNumbers[0], pageNumbers[1]));
+                }
+                else if (line.Contains(','))
+                {
+                    List<int> pageNumbers = line.Split(',').Select(number => int.Parse(number)).ToList();
+
+                    bool inOrder = true;
+
+                    for (int before = 0; before < pageNumbers.Count - 1; before++)
+                    {
+                        for (int after = before + 1; after < pageNumbers.Count; after++)
+                        {
+                            if (rules.Any(rule => rule.before == pageNumbers[after] && rule.after == pageNumbers[before]))
+                            {
+                                inOrder = false;
+                                (pageNumbers[after], pageNumbers[before]) = (pageNumbers[before], pageNumbers[after]);
+                            }
+                        }
+                    }
+
+                    if (inOrder)
+                    {
+                        sumOfCorrectMiddlePageNumbers += pageNumbers[(pageNumbers.Count - 1) / 2];
+                    }
+                    else
+                    {
+                        sumOfCorrectedMiddlePageNumbers += pageNumbers[(pageNumbers.Count - 1) / 2];
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
+            return $"{sumOfCorrectMiddlePageNumbers:N0} is the sum of the middle page numbers for the correctly-ordered updates\r\n" +
+                   $"{sumOfCorrectedMiddlePageNumbers:N0} is the sum of the middle page numbers for the incorrectly-ordered updates after correcting the order\r\n" +
+                   $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
-
     }
 }
