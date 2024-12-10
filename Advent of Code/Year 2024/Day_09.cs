0bce02d2330cb5ae03d266b20b88c84d6528e8be
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2024-12-09 20:09:08 -0600

    Year 2024 Day 9 Disk Fragmenter

--- Advent of Code/Year 2024/Day_09.cs
+++ Advent of Code/Year 2024/Day_09.cs
@@ -8,17 +8,100 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long filesystemChecksumMovingBlocks = 0;
+            long filesystemChecksumMovingFiles = 0;
+
+            Queue<int> diskmapFreeBlocks = [];
+            SortedDictionary<int, int> diskmapBlocks = [];
+
+            SortedDictionary<int, int> diskmapFreeFiles = [];
+            Dictionary<int, (int Blocks, int Position)> diskmapFiles = [];
+
+            int maxID = 0;
+            int positionBlocks = 0;
+            int positionFiles = 0;
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                for (int idx = 0; idx < line.Length; idx++)
+                {
+                    int digit = line[idx] - '0';
+
+                    if (idx % 2 == 0)
+                    {
+                        for (int i = 0; i < digit; i++)
+                        {
+                            diskmapBlocks.Add(positionBlocks++, maxID);
+                        }
+
+                        diskmapFiles.Add(maxID++, (digit, positionFiles));
+                    }
+                    else if (digit > 0)
+                    {
+                        for (int i = 0; i < digit; i++)
+                        {
+                            diskmapFreeBlocks.Enqueue(positionBlocks++);
+                        }
+
+                        diskmapFreeFiles.Add(positionFiles, digit);
+                    }
+
+                    positionFiles += digit;
+                }
+            }
+
+            // Part One
+
+            for (var last = diskmapBlocks.Last(); diskmapFreeBlocks.Count != 0 && last.Key > diskmapFreeBlocks.Peek(); last = diskmapBlocks.Last())
+            {
+                diskmapBlocks.Remove(last.Key);
+                diskmapBlocks.Add(diskmapFreeBlocks.Dequeue(), last.Value);
+            }
+
+            foreach (var block in diskmapBlocks)
+            {
+                filesystemChecksumMovingBlocks += block.Key * block.Value;
+            }
+
+            // Part Two
+
+            for (int id = maxID - 1; id >= 0; id--)
+            {
+                (int blocks, int oldPosition) = diskmapFiles[id];
+
+                if (diskmapFreeFiles.Any(map => map.Value >= blocks))
+                {
+                    var freeSpaces = diskmapFreeFiles.First(map => map.Value >= blocks);
+                    int newPosition = freeSpaces.Key;
+                    int freeBlocks = freeSpaces.Value;
+
+                    if (newPosition < oldPosition)
+                    {
+                        diskmapFreeFiles.Remove(newPosition);
+
+                        if (freeBlocks > blocks)
+                        {
+                            diskmapFreeFiles.Add(newPosition + blocks, freeBlocks - blocks);
+                        }
+
+                        diskmapFiles.Remove(id);
+                        diskmapFiles.Add(id, (blocks, newPosition));
+                    }
+                }
+            }
+
+            foreach (var file in diskmapFiles)
+            {
+                for (int block = 0; block < file.Value.Blocks; block++)
+                {
+                    filesystemChecksumMovingFiles += (file.Value.Position + block) * file.Key;
+                }
             }
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{filesystemChecksumMovingBlocks:N0} is the filesystem checksum when moving one block at a time\r\n" +
+                   $"{filesystemChecksumMovingFiles:N0} is the filesystem checksum when moving whole files\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
