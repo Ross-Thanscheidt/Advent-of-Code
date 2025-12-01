928cc3e9e52fc469a255fad1cac3601879b1d8f0
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-11-30 23:47:11 -0600

    Year 2025 Day 1 Secret Entrance

--- Advent of Code/Year 2025/Day_01.cs
+++ Advent of Code/Year 2025/Day_01.cs
@@ -8,17 +8,45 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long landingOnZeroCount = 0;
+            long landingOnOrPassingZeroCount = 0;
+
+            int dial = 50;
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                int direction = line[0] == 'L' ? -1 : 1;
+                int units = int.Parse(line[1..]);
+
+                for (int count = units; count > 0; count--)
+                {
+                    dial += direction;
+
+                    if (dial == -1)
+                    {
+                        dial = 99;
+                    }
+                    else if (dial == 100)
+                    {
+                        dial = 0;
+                    }
+
+                    if (dial == 0)
+                    {
+                        if (count == 1)
+                        {
+                            landingOnZeroCount++;
+                        }
+
+                        landingOnOrPassingZeroCount++;
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{landingOnZeroCount:N0} is the actual password to open the door\r\n" +
+                   $"{landingOnOrPassingZeroCount:N0} is the password to open the door using password method 0x434C49434B\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
