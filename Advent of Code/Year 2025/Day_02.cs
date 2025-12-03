8e074767fa092f98330765e4e836ab664e6fdb3f
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-12-02 22:46:43 -0600

    Year 2025 Day 2 Gift Shop

--- Advent of Code/Year 2025/Day_02.cs
+++ Advent of Code/Year 2025/Day_02.cs
@@ -8,17 +8,57 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long sum = 0;
+            long sumNewRules = 0;
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                foreach (string range in line.Split(','))
+                {
+                    string[] ranges = range.Split('-');
+                    long start = long.Parse(ranges[0]);
+                    long end = long.Parse(ranges[1]);
+
+                    for (long id = start; id <= end; id++)
+                    {
+                        string idString = id.ToString();
+
+                        bool invalid = false;
+                        bool invalidNewRules = false;
+
+                        for (int i = 0; i < idString.Length / 2 && (!invalid || !invalidNewRules); i++)
+                        {
+                            string sub = idString[..(i + 1)];
+                            string test = sub;
+
+                            while (test.Length < idString.Length)
+                            {
+                                test += sub;
+                            }
+
+                            if (test == idString)
+                            {
+                                if ((i + 1) * 2 == idString.Length && !invalid)
+                                {
+                                    sum += id;
+                                    invalid = true;
+                                }
+
+                                if (!invalidNewRules)
+                                {
+                                    sumNewRules += id;
+                                    invalidNewRules = true;
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{sum:N0} is the sum of all of the invalid IDs\r\n" +
+                   $"{sumNewRules:N0} is the sum of all of the invalid IDs using the new rules\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
