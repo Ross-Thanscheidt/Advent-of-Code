36a0a75ceea324e23a099820ff7d1ad3b392dd08
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-12-03 21:20:47 -0600

    Year 2025 Day 3 Lobby

--- Advent of Code/Year 2025/Day_03.cs
+++ Advent of Code/Year 2025/Day_03.cs
@@ -8,17 +8,45 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long joltage2 = 0;
+            long joltage12 = 0;
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                foreach (int batteriesPerBank in new int[] { 2, 12 })
+                {
+                    long joltage = 0;
+                    int currentLargest = 0;
+
+                    for (int digitsLeft = batteriesPerBank; digitsLeft > 0; digitsLeft--)
+                    {
+                        for (int i = currentLargest + 1; i < line.Length - (digitsLeft - 1); i++)
+                        {
+                            if (line[i] > line[currentLargest])
+                            {
+                                currentLargest = i;
+                            }
+                        }
+
+                        joltage = joltage * 10 + (line[currentLargest] - '0');
+                        currentLargest++;
+                    }
+
+                    if (batteriesPerBank == 2)
+                    {
+                        joltage2 += joltage;
+                    }
+                    else
+                    {
+                        joltage12 += joltage;
+                    }
+                }
             }
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{joltage2:N0} is the total output joltage with 2 batteries per bank\r\n" +
+                   $"{joltage12:N0} is the total output joltage with 12 batteries per bank\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
