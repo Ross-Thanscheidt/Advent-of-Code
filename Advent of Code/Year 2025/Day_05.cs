e61849fbdec5423d652046603a79437a07b72128
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-12-06 10:14:39 -0600

    Year 2025 Day 5 Cafeteria

--- Advent of Code/Year 2025/Day_05.cs
+++ Advent of Code/Year 2025/Day_05.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Range = Advent_of_Code.Year_2025_Day_05.Range;
 
 namespace Advent_of_Code
 {
@@ -8,17 +9,57 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long freshAvailableIngredients = 0;
+            long freshIngredientIDs = 0;
+
+            List<Range> ranges = [];
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                if (line.Contains('-'))
+                {
+                    List<string> boundaries = [.. line.Split('-')];
+                    ranges.Add(new(long.Parse(boundaries[0]), long.Parse(boundaries[1])));
+                }
+                else if (long.TryParse(line, out long id))
+                {
+                    if (ranges.Any(range => range.Start <= id && id <= range.End))
+                    {
+                        freshAvailableIngredients++;
+                    }
+                }
             }
+
+            Range[] rangeArray = [.. ranges];
+            int indexOfLastElement = rangeArray.Length - 1;
+
+            int i = 0;
+
+            while (i <= indexOfLastElement)
+            {
+                bool startOver = false;
+
+                for (int j = i + 1; j <= indexOfLastElement && !startOver; j++)
+                {
+                    if (rangeArray[i].Overlaps(rangeArray[j]) || rangeArray[i].Adjacent(rangeArray[j]))
+                    {
+                        rangeArray[i].Start = Math.Min(rangeArray[i].Start, rangeArray[j].Start);
+                        rangeArray[i].End = Math.Max(rangeArray[i].End, rangeArray[j].End);
+                        rangeArray.Skip(j + 1).Take(indexOfLastElement - j).ToArray().CopyTo(rangeArray, j);
+                        indexOfLastElement--;
+                        startOver = true;
+                    }
+                }
+
+                i = startOver ? 0 : i + 1;
+            }
+
+            freshIngredientIDs = rangeArray.Take(indexOfLastElement + 1).Sum(range => range.End - range.Start + 1);
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{freshAvailableIngredients:N0} available ingredient IDs are fresh\r\n" +
+                   $"{freshIngredientIDs:N0} ingredient IDs are considered to be fresh\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
---
+++ Advent of Code/Year 2025/Day_05_Range.cs
@@ -0,0 +1,15 @@
+﻿namespace Advent_of_Code.Year_2025_Day_05
+{
+    public struct Range(long start, long end)
+    {
+        public long Start { get; set; } = start;
+
+        public long End { get; set; } = end;
+
+        public bool Overlaps(Range other)
+            => this.Start <= other.End && this.End >= other.Start;
+
+        public bool Adjacent(Range other)
+            => this.End + 1 == other.Start || other.End + 1 == this.Start;
+    }
+}
