4941d4f4ae73d4a8d7c4d0132f2845f9de444969
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-12-13 18:10:44 -0600

    Year 2025 Day 8 Playground

--- Advent of Code/Year 2025/Day_08.cs
+++ Advent of Code/Year 2025/Day_08.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Advent_of_Code.Year_2025_Day_08;
 
 namespace Advent_of_Code
 {
@@ -8,17 +9,72 @@ namespace Advent_of_Code
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            long part1 = 0;
-            long part2 = 0;
+            long productOfLargestCircuits = 0;
+            long lastConnectionXProduct = 0;
+
+            List<Position> boxes = [];
 
             for (var line = input.ReadLine(); line != null; line = input.ReadLine())
             {
+                int[] coordinates = [.. line.Split(',').Select(s => int.Parse(s))];
+                boxes.Add(new(coordinates[0], coordinates[1], coordinates[2]));
+            }
+
+            List<(int Box1Index, int Box2Index, double Distance)> boxDistances = [];
+
+            for (int i = 0; i < boxes.Count - 1; i++)
+            {
+                for (int j = i + 1; j < boxes.Count; j++)
+                {
+                    boxDistances.Add((i, j, boxes[i].Distance(boxes[j])));
+                }
+            }
+
+            List<List<int>> circuits = [];
+
+            int connectionCount = 0;
+
+            foreach (var (box1, box2, distance) in boxDistances.OrderBy(nb => nb.Distance))
+            {
+                int circuitBox1 = circuits.FindIndex(circuit => circuit.Contains(box1));
+                int circuitBox2 = circuits.FindIndex(circuit => circuit.Contains(box2));
+
+                if (circuitBox1 == -1 && circuitBox2 == -1)
+                {
+                    circuits.Add([box1, box2]);
+                }
+                else if (circuitBox2 == -1)
+                {
+                    circuits[circuitBox1].Add(box2);
+                }
+                else if (circuitBox1 == -1)
+                {
+                    circuits[circuitBox2].Add(box1);
+                }
+                else if (circuitBox1 != circuitBox2)
+                {
+                    circuits[circuitBox1].AddRange(circuits[circuitBox2].Where(i => !circuits[circuitBox1].Contains(i)));
+                    circuits.RemoveAt(circuitBox2);
+                }
+
+                if (++connectionCount == (boxes.Count <= 20 ? 10 : 1_000))
+                {
+                    productOfLargestCircuits = circuits
+                        .OrderByDescending(circuit => circuit.Count).Take(3).Select(circuit => circuit.Count)
+                        .Aggregate(1, (product, count) => product * count);
+                }
+
+                if (circuits.Count == 1 && circuits[0].Count == boxes.Count)
+                {
+                    lastConnectionXProduct = boxes[box1].X * boxes[box2].X;
+                    break;
+                }
             }
 
             stopwatch.Stop();
 
-            return $"{part1:N0}\r\n" +
-                   $"{part2:N0}\r\n" +
+            return $"{productOfLargestCircuits:N0} is the product of the sizes of the three largest circuits\r\n" +
+                   $"{lastConnectionXProduct:N0} is the product of the X coordinates of the last two junction boxes to connect\r\n" +
                    $"({stopwatch.Elapsed.TotalMilliseconds} ms)";
         }
     }
---
+++ Advent of Code/Year 2025/Day_08_Position.cs
@@ -0,0 +1,27 @@
+﻿namespace Advent_of_Code.Year_2025_Day_08
+{
+    public struct Position(int x, int y, int z)
+    {
+        public int X { get; set; } = x;
+
+        public int Y { get; set; } = y;
+
+        public int Z { get; set; } = z;
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return left.X == right.X && left.Y == right.Y && left.Z == right.Z;
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+
+        public double Distance(Position other)
+            => Math.Sqrt(Math.Pow(other.X - this.X, 2) + Math.Pow(other.Y - this.Y, 2) + Math.Pow(other.Z - this.Z, 2));
+
+        //public double Distance(Position other)
+        //    => Math.Abs(other.X - this.X) + Math.Abs(other.Y - this.Y) + Math.Abs(other.Z - this.Z);
+    }
+}
